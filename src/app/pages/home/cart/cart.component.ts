81c29e93ad00ddf53f131cdd48c95662096d14e1
Omar Sayed <orhman@hotmail.com>
2022-08-21 00:59:58 +0200

    add basic cart item component

--- src/app/pages/home/cart/cart.component.ts
+++ src/app/pages/home/cart/cart.component.ts
@@ -1,27 +1,62 @@
+import { Subscription } from 'rxjs';
 import { LoginLogoutService } from './../../../services/login-logout.service';
 import { UserCart } from './../../../models/products';
 import { ProductsService } from './../../../services/products.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
   userCart: UserCart = {};
   userId: number | string = '';
+  loginSubscription: Subscription | undefined;
+  productSubscription: Subscription | undefined;
+  faBackward = faBackward;
+  itemCount = 0;
+  totalPrice = 0;
+
   constructor(
     private _ProductService: ProductsService,
     private _LoginLogoutService: LoginLogoutService
   ) {
-    _LoginLogoutService.userIdChange.subscribe((id) => {
-      this.userId = id;
-      _ProductService.userCartById(id).subscribe((cart) => {
-        this.userCart = cart;
-      });
-    });
+    this.loginSubscription = _LoginLogoutService.userIdChange.subscribe(
+      (id) => {
+        this.userId = id;
+        this.productSubscription = _ProductService
+          .userCartById(id)
+          .subscribe((cart) => {
+            this.userCart = cart;
+          });
+      }
+    );
   }
 
-  ngOnInit(): void {}
+  getItemCount() {
+    let totalCount = 0;
+    for (const id in this.userCart) {
+      totalCount += this.userCart[id].count;
+    }
+    this.itemCount = totalCount;
+  }
+  getTotalPrice() {
+    let totalPrice = 0;
+    for (const id in this.userCart) {
+      totalPrice += this.userCart[id].count * this.userCart[id].price;
+    }
+    this.totalPrice = totalPrice;
+  }
+
+  ngOnInit(): void {
+    this.getItemCount();
+    this.getTotalPrice();
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription?.unsubscribe;
+    this.productSubscription?.unsubscribe;
+  }
 }
--- src/app/pages/home/cart/cart.module.ts
+++ src/app/pages/home/cart/cart.module.ts
@@ -1,17 +1,21 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 
 @NgModule({
   declarations: [
-    CartComponent
+    CartComponent,
+    CartItemComponent
   ],
   imports: [
     CommonModule,
-    CartRoutingModule
+    CartRoutingModule,
+    FontAwesomeModule
   ]
 })
 export class CartModule { }

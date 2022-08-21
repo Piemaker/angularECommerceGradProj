52e79c2dfae6ff8d4c53c12f09523b7a5cc95357
Omar Sayed <orhman@hotmail.com>
2022-08-21 14:01:02 +0200

    Add cart badge

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,6 @@
+import { UserCart } from './../../models/products';
+import { Subscription } from 'rxjs';
+import { ProductsService } from './../../services/products.service';
 import { LoginLogoutService } from './../../services/login-logout.service';
 import {
   faCartShopping,
@@ -6,27 +9,57 @@ import {
   faUser,
   faVest,
 } from '@fortawesome/free-solid-svg-icons';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   faCartShopping = faCartShopping;
   faDoorOpen = faDoorOpen;
   faUser = faUser;
   faVest = faVest;
   faDoorClosed = faDoorClosed;
   isLogin = false;
-  constructor(private loginLogoutService: LoginLogoutService) {
-    this.loginLogoutService.userChange.subscribe((value) => {
+  itemCount: number = 0;
+  userId: number | string = '';
+  loginSubscription: Subscription | undefined;
+  productSubscription: Subscription | undefined;
+
+  constructor(
+    private _LoginLogoutService: LoginLogoutService,
+    private _ProductService: ProductsService
+  ) {
+    this._LoginLogoutService.userChange.subscribe((value) => {
       this.isLogin = !!value;
     });
+    this.loginSubscription = _LoginLogoutService.userIdChange.subscribe(
+      (id) => {
+        this.userId = id;
+        this.productSubscription = _ProductService
+          .userCartById(id)
+          .subscribe((cart) => {
+            this.getItemCartCount(cart);
+          });
+      }
+    );
   }
   ngOnInit(): void {}
+  getItemCartCount(cart: UserCart) {
+    let count = 0;
+    for (let id in cart) {
+      count += cart[id].count;
+    }
+    this.itemCount = count;
+  }
   logout() {
-    this.loginLogoutService.logoutUser();
+    this._LoginLogoutService.logoutUser();
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription?.unsubscribe;
+    this.productSubscription?.unsubscribe;
   }
 }
